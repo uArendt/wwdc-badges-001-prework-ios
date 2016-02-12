6f2654d921155c84588c08490207493854525dec
uArendt <jarendt66@me.com>
2016-02-12 13:28:01 -0500

    I redid the project and made two seperate arrays for first and last names

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -5,14 +5,27 @@
 @implementation FISAppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
     
-    /**
-     
-     * Write your solution here.
-     
-     */
+//    NSArray *anitaBorg = @[@"Anita", @"Borg"];
+//    NSArray *alanKay = @[@"Alan", @"Kay"];
+//    NSArray *adaLovelace = @[@"Ada", @"Lovelace"];
+//    NSArray *aaronSwartz = @[@"Aaron", @"Swartz"];
+//    NSArray *alanTuring = @[@"Alan", @"Turing"];
+//    NSArray *michaelFaraday = @[@"Michael", @"Faraday"];
+//    NSArray *graceHopper = @[@"Grace", @"hopper"];
+//    NSArray *charlesBabbage = @[@"Charles", @"Babbage"];
+
+    NSArray *speakers = @[@"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage"];
+    
+    NSArray *first = @[@"Anita", @"Alan", @"Ada", @"Aaron", @"Alan", @"Michael", @"Grace", @"Charles"];
+    NSArray *last = @[@"Borg", @"Kay", @"Lovelace", @"Swartz", @"Turing", @"Faraday", @"Hopper", @"Babbage"];
+    
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        NSLog(@"Hello, my name is, %@ %@!", first[i], last[i]);
+    }
+
     
     // do not alter
     return YES;  //
